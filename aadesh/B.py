f687e997e7cf6d26543b593fa1b0e9e901eb4165
Aadesh Manandhar <aadeshmanandhar@Aadesh-PC>
2024-10-24 17:50:31 +0545

    B.py

---
+++ aadesh/B.py
@@ -0,0 +1,42 @@
+testCases = int(input())
+
+for eachCase in range(testCases):
+    n, k = map(int, input().split(" "))
+    a = list(map(int, input().split(" ")))
+
+    a.sort()
+    
+    b  = list(set(a))
+
+
+    mem = {}
+
+    for slot in a:
+        if (slot not in mem):
+            mem[slot] = 1
+        else:
+            mem[slot] += 1
+
+
+    count = 0
+
+    runs = 0
+
+    index = 0
+
+    while k > 0:
+        if (n >= k or (n * b[index]) >= k):
+            count += k
+            k = 0
+        else:
+            count += (n * b[index])
+            k -= (n * b[index])
+            runs += b[index]
+            index += 1
+            
+            n -= mem[b[index]]
+            count += mem[b[index]]
+
+    
+
+    print(count)
